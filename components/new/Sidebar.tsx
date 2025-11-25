782f7da29bb62fa1a415e5473902e82fd4bdc3df
Imaadh Ifthikar <101471526+imaadh0@users.noreply.github.com>
2025-11-25 16:21:33 +0530

    fix: build errors

--- components/new/Sidebar.tsx
+++ components/new/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { X, ChevronDown, ChevronRight } from "lucide-react";
+import { X, ChevronDown } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "motion/react";
